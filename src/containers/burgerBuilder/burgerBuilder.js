6559570d11f637f7544093a1d029bc4e468f5eac
afsaneGhafouri <JUnia6468>
2019-08-01 18:45:54 +1000

    init project

---
+++ src/containers/burgerBuilder/burgerBuilder.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+import Aux from '../../hoc/aux';
+
+class burgerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Controlers</div>
+            </Aux>
+        );
+    }
+}
+
+
+export default burgerBuilder;
