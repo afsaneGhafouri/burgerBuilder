ce693563530225b583608f55dc42d6c094169b0a
afsaneGhafouri <JUnia6468>
2019-08-05 09:03:00 +1000

    add build controls on view

--- src/containers/burgerBuilder/burgerBuilder.js
+++ src/containers/burgerBuilder/burgerBuilder.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import Aux from '../../hoc/aux';
 import Burger from '../../components/burger/burger';
+import BuildControls from '../../components/burger/buildControls/buildControls';
 
 class burgerBuilder extends Component {
     state = {
@@ -10,18 +11,16 @@ class burgerBuilder extends Component {
             cheese: 0,
             meat: 0
         }
-    }
-
+    };
 
     render() {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger Controlers</div>
+                <BuildControls/>
             </Aux>
         );
     }
 }
-
 
 export default burgerBuilder;
